899eaf6507e9c11a52b3750b812a3b115c7078be
13232160100 <ypc0100@qq.com>
2022-12-04 21:53:51 +0800

    初始化

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://localhost:8080',
+          target: 'http://111.230.194.21:8080',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
